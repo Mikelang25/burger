4324c90b1ef0ede107a189fe72a2715ae6abeaf2
unknown <michaellang2525@yahoo.com>
2020-02-01 19:17:54 -0500

    creation of the file struction, creation of code for selecting all entries from the database

---
+++ config/orm.js
@@ -0,0 +1,22 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+
+    selectAll: function (table, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function (table,col,val,cb){
+
+    },
+    updateOne:function(table,objColVals,condition,cb){
+
+    }
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+USE burgers; 
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac",TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Quarter Pounder With Cheese",FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Single Cheeseburger",TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheeseburger",FALSE);
+
